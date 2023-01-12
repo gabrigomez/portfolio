092296eeb1a4a3b885639fb15e77faf7d8acf190
Gabriel <gabrigomez15@gmail.com>
2023-01-12 16:30:14 -0300

    feat: wip in main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,16 +4,29 @@ import './Main.css';
 
 export const Main = () => {
   return (
-    <div className='main-container'>
-      <div className='main-info'>
-        Main Info
+    <section className='main-container'>
+      <div className='main-card'>
+        <div className='main-info'>
+          <h3>
+            Bem-vindo ao meu portfólio!
+          </h3>
+          <h1>
+            Olá! Eu sou Gabriel Gomes!
+          </h1>
+          <h4>
+            Felis imperdiet proin fermentum leo vel orci porta. 
+            Purus faucibus ornare suspendisse sed nisi. 
+            Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare. 
+            Aenean euismod elementum nisi quis eleifend. Cursus metus aliquam eleifend mi in.
+          </h4>        
+        </div>
+        <div>
+          <img
+            className='main-gif' 
+            src={coding} 
+            alt="" />
+        </div>
       </div>
-      <div>
-        <img
-          className='main-gif' 
-          src={coding} 
-          alt="" />
-      </div>
-    </div>
+    </section>
   )
 }
