462ce394a34ddbfcde40592c81519b7c41084a3f
Gabriel <gabrigomez15@gmail.com>
2023-01-21 11:59:23 -0300

    fix: change footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Footer.css';
-import finnJake from '../assets/finn-jake.gif';
 
 export const Footer = () => {
   return (
     <section className='footer-container'>
-      <div className='footer-card'>
-        <img className='footer-image' src={finnJake} alt="" />        
+      <div className='footer-card'>                
         <p>develop by gabrigomez</p>
         <p>2022 - Todos os direitos reservados</p>        
       </div>
