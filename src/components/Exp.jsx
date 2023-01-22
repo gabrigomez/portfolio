51978aa5e65a233e977abe1c9d37ac01a9092d63
Gabriel <gabrigomez15@gmail.com>
2023-01-22 11:55:20 -0300

    feat: replace css to tailwind in exp component

--- src/components/Exp.jsx
+++ src/components/Exp.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Exp.css';
 import adeptly from '../assets/adeptly.jpeg';
 import tracklift from '../assets/Tracklift.jpeg';
 import 'animate.css';
@@ -7,40 +6,40 @@ import TrackVisibility from 'react-on-screen';
 
 export const Exp = () => {
   return (
-    <section className='exp-container' id='exp'>
-      <div className='exp-card'>
-        <div className='exp-title'>
-          <h1>
+    <section className='flex w-full justify-center bg-primaryBg' id='exp'>
+      <div className='flex flex-col w-2/3 p-16 rounded-2xl text-white'>
+        <div className='mb-10'>
+          <h1 className='text-6xl mb-2'>
             Experiência
           </h1>
           <h3>
             Essa é a minha jornada como Dev (até aqui)
           </h3>      
         </div>
-        <div className='exp-info'>
+        <div className='flex justify-center'>
           <TrackVisibility>
             {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : "none"}>
-                <div className='exp-info-items'>
-                  <div className='exp-item'>          
+                <div className='flex'>
+                  <div className='flex flex-col items-center h-96 w-64 p-4 m-1 rounded-lg hover:bg-secondaryBg duration-500 group'>          
                     <img 
                       src={adeptly} 
                       alt=""
-                      className='exp-image' 
+                      className='border-2 border-solid border-primary h-36 w-36' 
                     />
-                    <h3>Adeptly</h3>
-                    <p className='exp-year'>Ago 2021 - Jan 2023</p>
-                    <p className='exp-function'>Front-end developer/Quality Assurance</p>
+                    <h3 className='m-2 text-lg'>Adeptly</h3>
+                    <p className='opacity-0 group-hover:opacity-100 duration-500 mb-2'>Ago 2021 - Jan 2023</p>
+                    <p className='opacity-0 group-hover:opacity-100 duration-500 font-bold'>Front-end developer/Quality Assurance</p>
                   </div>
-                  <div className='exp-item'>          
+                  <div className='flex flex-col items-center h-96 w-64 p-4 m-1 rounded-lg hover:bg-secondaryBg duration-500 group'>          
                     <img 
                       src={tracklift} 
                       alt=""
-                      className='exp-image' 
+                      className='border-2 border-solid border-primary h-36 w-36' 
                     />
-                    <h3>Tracklift</h3>
-                    <p className='exp-year'>Jun 2021 - Ago 2021</p>
-                    <p className='exp-function'>Front-end developer</p>
+                    <h3 className='m-2'>Tracklift</h3>
+                    <p className='opacity-0 group-hover:opacity-100 duration-500 mb-2'>Jun 2021 - Ago 2021</p>
+                    <p className='opacity-0 group-hover:opacity-100 duration-500 font-bold'>Front-end developer</p>
                   </div>
                 </div>          
               </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBg: 'rgba(0, 0, 0, 0.562)',
+        secondaryBg: 'rgba(20, 20, 20, 0.514)',
+        primary: '#0083c99a',
+      },
+    },
   },
   plugins: [],
 }
