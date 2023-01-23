8ecaf953b5bd5ba12429440acb9f4d1d8ab24e11
Gabriel <gabrigomez15@gmail.com>
2023-01-23 13:13:42 -0300

    fix: responsiveness in exp component

--- src/components/Exp.jsx
+++ src/components/Exp.jsx
@@ -16,28 +16,28 @@ export const Exp = () => {
             Essa é a minha jornada como Dev (até aqui)
           </h3>      
         </div>
-        <div className='flex justify-center'>
-          <TrackVisibility>
+        <div className='flex justify-center w-full'>
+          <TrackVisibility className='w-full flex justify-center'>
             {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : "none"}>
                 <div className='flex h-96'>
-                  <div className='flex flex-col items-center h-4/5 w-32 md:h-96 md:w-64 p-4 m-1 rounded-lg hover:bg-secondaryBg duration-500 group'>          
+                  <div className='flex flex-col items-center h-4/5 w-2/4 md:h-96 md:w-64 p-4 m-1 rounded-lg hover:bg-secondaryBg duration-500 group'>          
                     <img 
                       src={adeptly} 
                       alt=""
                       className='border-2 border-solid border-primary h-20 w-20 md:h-36 md:w-36' 
                     />
-                    <h3 className='m-2 text-lg'>Adeptly</h3>
+                    <h3 className='m-2 text-2xl md:text-4xl'>Adeptly</h3>
                     <p className='opacity-0 group-hover:opacity-100 duration-500 mb-2'>Ago 2021 - Jan 2023</p>
                     <p className='opacity-0 group-hover:opacity-100 duration-500 font-bold'>Front-end developer/Quality Assurance</p>
                   </div>
-                  <div className='flex flex-col items-center h-4/5 w-32 md:h-96 md:w-64 p-4 m-1 rounded-lg hover:bg-secondaryBg duration-500 group'>          
+                  <div className='flex flex-col items-center h-4/5 w-2/4 md:h-96 md:w-64 p-4 m-1 rounded-lg hover:bg-secondaryBg duration-500 group'>          
                     <img 
                       src={tracklift} 
                       alt=""
                       className='border-2 border-solid border-primary h-20 w-20 md:h-36 md:w-36' 
                     />
-                    <h3 className='m-2'>Tracklift</h3>
+                    <h3 className='m-2 text-2xl md:text-4xl'>Tracklift</h3>
                     <p className='opacity-0 group-hover:opacity-100 duration-500 mb-2'>Jun 2021 - Ago 2021</p>
                     <p className='opacity-0 group-hover:opacity-100 duration-500 font-bold'>Front-end developer</p>
                   </div>
