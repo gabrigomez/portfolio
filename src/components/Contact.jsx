25626125acc7aeec7d8b0104f9e8838fea7f5b74
Gabriel <gabrigomez15@gmail.com>
2023-01-23 17:37:21 -0300

    feat: start to add responsiveness to contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,12 @@ import coding from '../assets/coding.gif';
 
 export const Contact = () => {
   return (
-    <section className='flex w-full justify-center mb-10' id='contact'>
-      <div className='flex justify-center bg-slate-50 w-2/4 rounded-lg px-20 py-20 mx-20 my-20'>
+    <section className='flex w-full justify-center mb-4' id='contact'>
+      <div 
+        className='flex flex-col 2xl:flex-row justify-center bg-slate-50 w-full md:w-3/4 rounded-lg py-4 px-4 mx-4 my-4 2xl:px-20 2xl:py-20 2xl:mx-20 2xl:my-20'
+      >
         <div className='flex justify-center items-center'>
-          <img className='h-72 w-72 border-2 rounded-full mr-5' src={coding} alt="" />
+          <img className='w-44 h-44 mb-4 lg:h-72 lg:w-72 border-2 rounded-full lg:mr-5' src={coding} alt="" />
         </div>
         <div className='flex flex-col justify-evenly ml-15'>
           <h1 className='text-3xl mb-4 font-bold'>
