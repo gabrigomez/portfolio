1d19b77194a1aa4a6ea8976a418560ce38586de6
Gabriel <gabrigomez15@gmail.com>
2023-01-23 12:02:52 -0300

    feat: replace css to tailwind in skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import './Skills.css';
+
 
 export const Skills = () => {
   const responsive = {
@@ -24,75 +24,99 @@ export const Skills = () => {
   };
 
   return (
-    <section className='skills-container' id='skills'>
-      <div className='skills-card'>
-        <h1>
+    <section className='flex justify-center w-full bg-primaryBg' id='skills'>
+      <div className='w-4/5 rounded-2xl p-16 text-white'>
+        <h1 className='text-6xl'>
           Skills
         </h1>
-        <h3>
+        <h3 className='mb-10'>
           Aqui estão os meus cursos e certificações
         </h3>
-        <Carousel responsive={responsive}>
-          <div className='skills-item'>
+        <Carousel responsive={responsive} className='h-3/4'>
+          <div className='flex flex-col items-center p-2 h-full m-2 hover:bg-secondaryBg rounded-3xl group'>
             <img 
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQagE2GtpuY9L4m-9yugjHD59o2UBCqpWzzhDBZ6NDB_UiLJLrEg6dh8J4CE8ZPmKX_FYE&usqp=CAU" 
               alt=""
-              className='skill-img' 
+              className='w-44 h-44 border-2 border-primary rounded-full mb-2' 
             />
-            <h3>Curso Técnico em Informática</h3>
-            <p>Concluído em 2014</p>                        
+            <h3 className='text-lg mb-2 font-bold'>Curso Técnico em Informática</h3>
+            <p className='opacity-0 group-hover:opacity-100 duration-500'>Concluído em 2014</p>                        
           </div>
-          <div className='skills-item'>
-            <img src="https://pbs.twimg.com/profile_images/1415325711896825858/2cczQq-X_400x400.png" 
-              alt=""
-              className='skill-img' 
-            />
-            <h3>The Complete Ruby on Rails Developer Course</h3>
-            <a href="https://www.udemy.com/course/the-complete-ruby-on-rails-developer-course/">Em andamento</a>
-          </div>
-          <div className='skills-item'>
-            <img src="https://pbs.twimg.com/profile_images/1415325711896825858/2cczQq-X_400x400.png" 
-              alt=""
-              className='skill-img' 
-            />
-            <h3>Curso Web Moderno Completo com JavaScript 2020</h3>
-            <a href="https://www.udemy.com/certificate/UC-39998402-6d77-4454-9d43-926719ec4091/">Certificado</a>
-          </div>          
-          <div className='skills-item'>
+          <div className='flex flex-col items-center p-2 h-full m-2 hover:bg-secondaryBg rounded-3xl group'>
             <img 
               src="https://pbs.twimg.com/profile_images/1415325711896825858/2cczQq-X_400x400.png" 
               alt=""
-              className='skill-img' 
+              className='w-44 h-44 border-2 border-primary rounded-full mb-2' 
             />
-            <h3>Testes de aplicações modernas com Cypress</h3>
-            <a href="https://www.udemy.com/certificate/UC-19b74e53-8f2e-46d1-a19a-c56d64196add/">Certificado</a>            
+            <h3 className='text-lg mb-2 font-bold'>The Complete Ruby on Rails Developer Course</h3>
+            <a 
+              href="https://www.udemy.com/course/the-complete-ruby-on-rails-developer-course/"
+              className='hover:text-primary opacity-0 group-hover:opacity-100 duration-500'>
+                Em andamento
+            </a>
           </div>
-          <div className='skills-item'>
-            <img src="https://pbs.twimg.com/profile_images/1415325711896825858/2cczQq-X_400x400.png" 
+          <div className='flex flex-col items-center p-2 h-full m-2 hover:bg-secondaryBg rounded-3xl group'>
+            <img 
+              src="https://pbs.twimg.com/profile_images/1415325711896825858/2cczQq-X_400x400.png" 
               alt=""
-              className='skill-img' 
+              className='w-44 h-44 border-2 border-primary rounded-full mb-2' 
             />
-            <h3>Entendendo Typescript</h3>
-            <a href="https://www.udemy.com/certificate/UC-0dce5ff9-7176-46b3-bf12-3a0253bff2af/">Certificado</a>
-          </div>
-          <div className='skills-item'>
-            <img src="https://pbs.twimg.com/profile_images/1415325711896825858/2cczQq-X_400x400.png" 
+            <h3 className='text-lg mb-2 font-bold'>Curso Web Moderno Completo com JavaScript 2020</h3>
+            <a 
+              href="https://www.udemy.com/certificate/UC-39998402-6d77-4454-9d43-926719ec4091/"
+              className='hover:text-primary opacity-0 group-hover:opacity-100 duration-500'>
+                Certificado
+            </a>
+          </div>          
+          <div className='flex flex-col items-center p-2 h-full m-2 hover:bg-secondaryBg rounded-3xl group'>
+            <img 
+              src="https://pbs.twimg.com/profile_images/1415325711896825858/2cczQq-X_400x400.png" 
               alt=""
-              className='skill-img' 
+              className='w-44 h-44 border-2 border-primary rounded-full mb-2' 
             />
-            <h3>Docker Completo do ZERO ao Avançado</h3>
-            <a href="https://www.udemy.com/certificate/UC-75b54042-1a9a-4703-a6dd-c03553950c23/">Certificado</a>
+            <h3 className='text-lg mb-2 font-bold'>Testes de aplicações modernas com Cypress</h3>
+            <a 
+              href="https://www.udemy.com/certificate/UC-19b74e53-8f2e-46d1-a19a-c56d64196add/"
+              className='hover:text-primary opacity-0 group-hover:opacity-100 duration-500'>
+                Certificado
+            </a>            
           </div>
-          <div className='skills-item'>
+          <div className='flex flex-col items-center p-2 h-full m-2 hover:bg-secondaryBg rounded-3xl group'>
+            <img 
+              src="https://pbs.twimg.com/profile_images/1415325711896825858/2cczQq-X_400x400.png" 
+              alt=""
+              className='w-44 h-44 border-2 border-primary rounded-full mb-2' 
+            />
+            <h3 className='text-lg mb-2 font-bold'>Entendendo Typescript</h3>
+            <a 
+              href="https://www.udemy.com/certificate/UC-0dce5ff9-7176-46b3-bf12-3a0253bff2af/"
+              className='hover:text-primary opacity-0 group-hover:opacity-100 duration-500'>
+                Certificado
+            </a>
+          </div>
+          <div className='flex flex-col items-center p-2 h-full m-2 hover:bg-secondaryBg rounded-3xl group'>
+            <img 
+              src="https://pbs.twimg.com/profile_images/1415325711896825858/2cczQq-X_400x400.png" 
+              alt=""
+              className='w-44 h-44 border-2 border-primary rounded-full mb-2' 
+            />
+            <h3 className='text-lg mb-2 font-bold'>Docker Completo do ZERO ao Avançado</h3>
+            <a 
+              href="https://www.udemy.com/certificate/UC-75b54042-1a9a-4703-a6dd-c03553950c23/"
+              className='hover:text-primary opacity-0 group-hover:opacity-100 duration-500'>
+                Certificado
+            </a>
+          </div>
+          <div className='flex flex-col items-center p-2 h-full m-2 hover:bg-secondaryBg rounded-3xl group'>
             <img 
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQagE2GtpuY9L4m-9yugjHD59o2UBCqpWzzhDBZ6NDB_UiLJLrEg6dh8J4CE8ZPmKX_FYE&usqp=CAU" 
               alt=""
-              className='skill-img' 
+              className='w-44 h-44 border-2 border-primary rounded-full mb-2' 
             />
-            <h3>Licenciatura em Letras</h3>
-            <a href="https://www.udemy.com/certificate/UC-75b54042-1a9a-4703-a6dd-c03553950c23/">Concluído em 2020</a>                        
+            <h3 className='text-lg mb-2 font-bold'>Licenciatura em Letras</h3>
+            <p className='opacity-0 group-hover:opacity-100 duration-500'>Concluído em 2020</p>                        
           </div>   
-        </Carousel>;
+        </Carousel>
       </div>
     </section>
   )
