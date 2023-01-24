8a8f81102127f30adbfe8fbbf14c2204712ae6f8
Gabriel <gabrigomez15@gmail.com>
2023-01-24 10:19:06 -0300

    feat: replace css to tailwind in footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import './Footer.css';
 
 export const Footer = () => {
   return (
-    <section className='footer-container'>
-      <div className='footer-card'>                
-        <p>develop by gabrigomez</p>
-        <p>2022 - Todos os direitos reservados</p>        
+    <section className='flex p-10 justify-center text-white hover:scale-105 duration-200 group'>
+      <div className='flex flex-col items-center w-64 p-5 rounded-2xl border border-gray-700 group-hover:border-gray-800'>                
+        <p className='mb-2 border-b border-black group-hover:border-gray-700 duration-300 '>
+          develop by gabrigomez
+        </p>
+        <p>2023 - Todos os direitos reservados</p>        
       </div>
     </section>
   )
