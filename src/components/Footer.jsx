a080b01beb0ba06d6fa0c55c813619b854744575
Gabriel <gabrigomez15@gmail.com>
2023-01-24 10:25:25 -0300

    fix: footer style adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <section className='flex p-10 justify-center text-white hover:scale-105 duration-200 group'>
-      <div className='flex flex-col items-center w-64 p-5 rounded-2xl border border-gray-700 group-hover:border-gray-800'>                
+    <footer className='flex p-10 justify-center text-white'>
+      <div className='flex flex-col items-center w-96 p-4 hover:scale-105 duration-200 rounded-2xl border border-gray-700 hover:border-gray-800 group'>                
         <p className='mb-2 border-b border-black group-hover:border-gray-700 duration-300 '>
           develop by gabrigomez
         </p>
         <p>2023 - Todos os direitos reservados</p>        
       </div>
-    </section>
+    </footer>
   )
 }
