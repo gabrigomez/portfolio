27c0c847d02acf14b010c1579691e44f851e069f
Gabriel <gabrigomez15@gmail.com>
2023-01-24 10:46:43 -0300

    fix: padding in project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,13 +19,13 @@ export const Projects = () => {
           {({ isVisible }) =>
             <div className={isVisible ? "animate__animated animate__fadeInLeft md:w-3/4" : "none"}>
               <div className='flex justify-center text-white'>
-                <div className='flex flex-col items-center m-2 p-2 h-54 w-2/4 md:w-64 duration-300 rounded-2xl hover:h-72 hover:bg-secondaryBg group'>
+                <div className='flex flex-col items-center m-2 p-2 pb-10 w-2/4 md:w-64 duration-300 rounded-2xl hover:bg-secondaryBg group'>
                   <img 
                     src="https://media3.giphy.com/media/gYWeVOiMmbg3kzCTq5/200w.gif?cid=6c09b952nv75vq5cbhhrd2g1vyqpqwf6m3o990h5iexcggxu&rid=200w.gif&ct=g" 
                     alt=""
                     className='w-44 h-44 border-2 border-primary rounded-full mb-4' 
                   />
-                  <p className='text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'>
+                  <p className='text-xl md:text-3xl font-bold opacity-0 group-hover:opacity-100 duration-700'>
                     My classic games
                   </p>
                   <a 
@@ -34,13 +34,13 @@ export const Projects = () => {
                       <GithubLogo className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 text-center hover:text-primary'/>
                   </a>                     
                 </div>
-                <div className='flex flex-col items-center m-2 p-2 h-54 w-2/4 md:w-64 duration-300 rounded-2xl hover:h-72 hover:bg-secondaryBg group'>
+                <div className='flex flex-col items-center m-2 p-2 pb-10 w-2/4 md:w-64 duration-300 rounded-2xl hover:bg-secondaryBg group'>
                   <img 
                     src="https://camo.githubusercontent.com/24b74a4d1a25c46e8d68cb58bba4df3de1059a53f0ef85d9a4be281e7a69a887/68747470733a2f2f7468756d62732e6766796361742e636f6d2f50696e6b5069657263696e6742756c6c2d73697a655f726573747269637465642e676966" 
                     alt=""
                     className='w-44 h-44 border-2 border-primary rounded-full mb-4' 
                   />
-                  <p className='text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'>
+                  <p className='text-xl md:text-3xl font-bold opacity-0 group-hover:opacity-100 duration-700'>
                     Vue login
                   </p>            
                   <a 
