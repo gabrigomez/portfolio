3efa0f25ad514915b4f72e02fa770716e7229a57
Gabriel <gabrigomez15@gmail.com>
2023-01-25 10:42:55 -0300

    feat: add multitheme to navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,8 +32,8 @@ export const NavBar = () => {
         <a 
           className='flex items-center no-underline group cursor-pointer' href='#home'
           onClick={() => onUpdateActiveLink('home')}>
-            <FinnTheHuman className='opacity-70 group-hover:opacity-100 w-10 h-10 sm:mr-2 text-primary dark:text-white' />
-            <p className='text-primary dark:text-white font-bold hidden sm:block opacity-60 group-hover:opacity-100 duration-300'>HOME</p>
+            <FinnTheHuman className='text-primary dark:text-white opacity-70 group-hover:opacity-100 w-10 h-10 sm:mr-2' />
+            <p className='text-primary dark:text-white font-bold hidden sm:block opacity-90 group-hover:opacity-100 duration-300'>HOME</p>
         </a>
         <BsSun className={`text-white cursor-pointer ${theme ? '' : 'hidden'}`} onClick={() => setTheme(!theme)} />
         <BsMoonFill className={`text-primary cursor-pointer ${theme ? 'hidden' : ''}`} onClick={() => setTheme(!theme)} />        
@@ -41,25 +41,25 @@ export const NavBar = () => {
           <div className='flex items-center'>            
             <a 
               href="#skills" 
-              className='text-primary font-bold dark:text-white px-2 sm:px-4 py-0 no-underline text-lg opacity-70 hover:opacity-100 cursor-pointer' 
+              className='text-primary font-bold dark:text-white px-2 sm:px-4 py-0 no-underline text-lg opacity-90 hover:opacity-100 cursor-pointer' 
               onClick={() => onUpdateActiveLink('skills')}>
                 Skills
             </a>
             <a
               href="#exp" 
-              className='text-white px-2 sm:px-4 py-0 no-underline text-lg opacity-50 hover:opacity-100 hover:text-white cursor-pointer' 
+              className='text-primary font-bold dark:text-white px-2 sm:px-4 py-0 no-underline text-lg opacity-90 hover:opacity-100 cursor-pointer' 
               onClick={() => onUpdateActiveLink('exp')}>
                 Exp
             </a>
             <a 
               href="#projects" 
-              className='text-white px-2 sm:px-4 py-0 no-underline text-lg opacity-50 hover:opacity-100 hover:text-white cursor-pointer' 
+              className='text-primary font-bold dark:text-white px-2 sm:px-4 py-0 no-underline text-lg opacity-90 hover:opacity-100 cursor-pointer' 
               onClick={() => onUpdateActiveLink('projects')}>
                 Projects
             </a>
             <a 
               href="#contact" 
-              className='text-white px-2 sm:px-4 py-0 no-underline text-lg opacity-50 hover:opacity-100 hover:text-white cursor-pointer' 
+              className='text-primary font-bold dark:text-white px-2 sm:px-4 py-0 no-underline text-lg opacity-90 hover:opacity-100 cursor-pointer' 
               onClick={() => onUpdateActiveLink('contact')}>
                 Contact
             </a>
@@ -68,13 +68,13 @@ export const NavBar = () => {
             <div>
               <a 
                 href="https://www.linkedin.com/in/gabrielgomes93/" 
-                className='social-icon'>
-                  <LinkedinLogo className='text-2xl z-10 duration-300 ease-in-out text-white hover:scale-105 hover:text-primary'/>
+                className='social-icon group'>
+                  <LinkedinLogo className='text-2xl z-10 duration-300 text-white hover:scale-105 group-hover:text-white'/>
               </a>
               <a 
                 href="https://github.com/gabrigomez"
-                className='social-icon'>
-                  <GithubLogo className='text-2xl z-10 duration-300 ease-in-out text-white hover:scale-105 hover:text-primary'/>
+                className='social-icon group'>
+                  <GithubLogo className='text-2xl z-10 duration-300 text-white hover:scale-105 group-hover:text-white'/>
               </a>
             </div>
           </div>
