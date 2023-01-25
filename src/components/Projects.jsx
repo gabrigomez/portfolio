a0ca4f02416d154249bfab06f8c81575db6edef5
Gabriel <gabrigomez15@gmail.com>
2023-01-25 11:13:48 -0300

    feat: add multitheme to projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,9 +5,9 @@ import { GithubLogo } from 'phosphor-react';
 
 export const Projects = () => {
   return (
-    <section className='flex flex-col items-center w-full h-[650px] mb-10 bg-primaryBg' id='projects'>      
+    <section className='flex flex-col items-center w-full h-[650px] mb-10 bg-gray-300 dark:bg-primaryBg' id='projects'>      
       <div className='flex flex-col items-center w-full p-2 mt-2 md:p-16 rounded-2xl'>
-        <div className='mb-20 text-white'>
+        <div className='mb-20 text-primary dark:text-white'>
           <h1 className='text-4xl md:text-6xl'>
             Projetos
           </h1>
@@ -18,8 +18,8 @@ export const Projects = () => {
         <TrackVisibility className='flex justify-center w-full'>
           {({ isVisible }) =>
             <div className={isVisible ? "animate__animated animate__fadeInLeft md:w-3/4" : "none"}>
-              <div className='flex justify-center text-white'>
-                <div className='flex flex-col items-center m-2 p-2 pb-10 w-2/4 md:w-64 duration-300 rounded-2xl hover:bg-secondaryBg group'>
+              <div className='flex justify-center text-primary dark:text-white'>
+                <div className='flex flex-col items-center m-2 p-2 pb-10 w-2/4 md:w-64 duration-300 rounded-2xl hover:bg-gray-200 dark:hover:bg-secondaryBg group'>
                   <img 
                     src="https://media3.giphy.com/media/gYWeVOiMmbg3kzCTq5/200w.gif?cid=6c09b952nv75vq5cbhhrd2g1vyqpqwf6m3o990h5iexcggxu&rid=200w.gif&ct=g" 
                     alt=""
@@ -31,10 +31,10 @@ export const Projects = () => {
                   <a 
                     href="https://github.com/gabrigomez/my-classic-games"
                     className='flex justify-center'>
-                      <GithubLogo className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 text-center hover:text-primary'/>
+                      <GithubLogo className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 text-center hover:text-black dark:hover:text-primary'/>
                   </a>                     
                 </div>
-                <div className='flex flex-col items-center m-2 p-2 pb-10 w-2/4 md:w-64 duration-300 rounded-2xl hover:bg-secondaryBg group'>
+                <div className='flex flex-col items-center m-2 p-2 pb-10 w-2/4 md:w-64 duration-300 rounded-2xl hover:bg-gray-200 dark:hover:bg-secondaryBg group'>
                   <img 
                     src="https://camo.githubusercontent.com/24b74a4d1a25c46e8d68cb58bba4df3de1059a53f0ef85d9a4be281e7a69a887/68747470733a2f2f7468756d62732e6766796361742e636f6d2f50696e6b5069657263696e6742756c6c2d73697a655f726573747269637465642e676966" 
                     alt=""
@@ -46,7 +46,7 @@ export const Projects = () => {
                   <a 
                     href="https://github.com/gabrigomez/login-page"
                     className='flex justify-center'>
-                      <GithubLogo className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 text-center hover:text-primary'/>
+                      <GithubLogo className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 text-center hover:text-black dark:hover:text-primary'/>
                   </a>                       
                 </div>
               </div>
