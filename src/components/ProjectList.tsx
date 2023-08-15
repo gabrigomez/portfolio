9172543ec5f50a1241d66e63fe13b1d21e8a9c51
Gabriel <gabrigomez15@gmail.com>
2023-08-15 17:02:43 -0300

    fix: code indentation

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -35,172 +35,172 @@ export const ProjectList = () => {
     <Card.Root className='flex flex-wrap w-4/5 justify-center text-primary dark:text-white'>
       <Carousel responsive={responsive} className='w-full'>
         <div className='project-list-container group'>
-            <Card.CardImage
-              src={SpotifyGif} 
-              className='w-22 h-22 border-2 bg-white border-primary rounded-full mb-4' 
-            />
-            <Card.CardTitle 
-              className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
-              title='My Birthday Show'
-            />
-            <div className='flex'>
-              <a 
-                target="blank"
-                href="https://github.com/gabrigomez/my-birthday-show"
-                className='flex justify-center mr-2'>
-                  <GithubLogo 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a> 
-              <a 
-                target="blank"
-                href="https://mybirthdayshow.netlify.app/"
-                className='flex justify-center'>
-                  <Browsers 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a>
-            </div>                                          
-        </div>
-        <div className='project-list-container group'>
-            <Card.CardImage
-              src={Music} 
-              className='w-22 h-22 border-2 bg-white border-primary rounded-full mb-4' 
-            />
-            <Card.CardTitle 
-              className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
-              title="Tr4cker"
-            />
-            <div className='flex'>
-              <a 
-                target="blank"
-                href="https://github.com/gabrigomez/tr4cker"
-                className='flex justify-center mr-2'>
-                  <GithubLogo 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a>  
-              <a 
-                target="blank"
-                href="https://tr4cker.netlify.app/"
-                className='flex justify-center'>
-                  <Browsers 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a>
-            </div>                               
-        </div>
-        <div className='project-list-container group'>
-            <Card.CardImage
-              src={Chat} 
-              className='w-22 h-22 border-2 border-primary rounded-full mb-4' 
-            />
-            <Card.CardTitle 
-              className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
-              title="Let's Chat!"
-            />
-            <div className='flex'>
-              <a 
-                target="blank"
-                href="https://github.com/gabrigomez/lets-chat"
-                className='flex justify-center mr-2'>
-                  <GithubLogo 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a>  
-              <a 
-                target="blank"
-                href="https://letschatoficial.netlify.app/"
-                className='flex justify-center'>
-                  <Browsers 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a>
-            </div>                               
-        </div>
-        <div className='project-list-container group'>
-            <Card.CardImage
-              src={Disappointed} 
-              className='w-22 h-22 border-2 bg-white border-primary rounded-full mb-4' 
-            />
-            <Card.CardTitle 
-              className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
-              title="Esqueci meu CEP!"
-            />              
-            <div className='flex'>
-              <a 
-                target="blank"
-                href="https://github.com/gabrigomez/esqueci-meu-cep"
-                className='flex justify-center mr-2'>
-                  <GithubLogo 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a>  
-              <a 
-                target="blank"
-                href="https://esquecimeucep.netlify.app/"
-                className='flex justify-center'>
-                  <Browsers 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a>
-            </div>                               
-        </div>
-        <div className='project-list-container group'>
-            <Card.CardImage
-              src={GithubGif} 
-              className='w-22 h-22 border-2 border-primary rounded-full mb-4' 
-            />
-            <Card.CardTitle 
-              className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
-              title="Github Repo Filter"
-            />                                  
-            <div className='flex'>
-              <a 
-                target="blank"
-                href="https://github.com/gabrigomez/github-repo-finder"
-                className='flex justify-center mr-2'>
-                  <GithubLogo 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a>
-              <a 
-                target="blank"
-                href="https://finderrepo.netlify.app/"
-                className='flex justify-center'>
-                  <Browsers 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary'/>
-              </a>
-            </div>                                         
-        </div>        
-        <div className='project-list-container group'>
-            <Card.CardImage
-              src={Pacman} 
-              className='w-22 h-22 border-2 border-primary rounded-full mb-4' 
-            />
-            <Card.CardTitle 
-              className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
-              title="My classic games"
-            />              
-            <div className='flex'>            
-              <a 
-                target="blank"
-                href="https://github.com/gabrigomez/my-classic-games"
-                className='flex justify-center'>
-                  <GithubLogo 
-                    className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
-                    text-center hover:text-black dark:hover:text-primary mr-2'/>
-              </a>
-              <a 
-                target="blank"
-                href="https://myclassicgamelist.netlify.app/"
-                className='flex justify-center'>
+          <Card.CardImage
+            src={SpotifyGif} 
+            className='w-22 h-22 border-2 bg-white border-primary rounded-full mb-4' 
+          />
+          <Card.CardTitle 
+            className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
+            title='My Birthday Show'
+          />
+          <div className='flex'>
+            <a 
+              target="blank"
+              href="https://github.com/gabrigomez/my-birthday-show"
+              className='flex justify-center mr-2'>
+                <GithubLogo 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary'/>
+            </a> 
+            <a 
+              target="blank"
+              href="https://mybirthdayshow.netlify.app/"
+              className='flex justify-center'>
                 <Browsers 
                   className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
                   text-center hover:text-black dark:hover:text-primary'/>
-              </a>
-            </div>                     
+            </a>
+          </div>                                          
+        </div>
+        <div className='project-list-container group'>
+          <Card.CardImage
+            src={Music} 
+            className='w-22 h-22 border-2 bg-white border-primary rounded-full mb-4' 
+          />
+          <Card.CardTitle 
+            className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
+            title="Tr4cker"
+          />
+          <div className='flex'>
+            <a 
+              target="blank"
+              href="https://github.com/gabrigomez/tr4cker"
+              className='flex justify-center mr-2'>
+                <GithubLogo 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary'/>
+            </a>  
+            <a 
+              target="blank"
+              href="https://tr4cker.netlify.app/"
+              className='flex justify-center'>
+                <Browsers 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary'/>
+            </a>
+          </div>                               
+        </div>
+        <div className='project-list-container group'>
+          <Card.CardImage
+            src={Chat} 
+            className='w-22 h-22 border-2 border-primary rounded-full mb-4' 
+          />
+          <Card.CardTitle 
+            className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
+            title="Let's Chat!"
+          />
+          <div className='flex'>
+            <a 
+              target="blank"
+              href="https://github.com/gabrigomez/lets-chat"
+              className='flex justify-center mr-2'>
+                <GithubLogo 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary'/>
+            </a>  
+            <a 
+              target="blank"
+              href="https://letschatoficial.netlify.app/"
+              className='flex justify-center'>
+                <Browsers 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary'/>
+            </a>
+          </div>                               
+        </div>
+        <div className='project-list-container group'>
+          <Card.CardImage
+            src={Disappointed} 
+            className='w-22 h-22 border-2 bg-white border-primary rounded-full mb-4' 
+          />
+          <Card.CardTitle 
+            className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
+            title="Esqueci meu CEP!"
+          />              
+          <div className='flex'>
+            <a 
+              target="blank"
+              href="https://github.com/gabrigomez/esqueci-meu-cep"
+              className='flex justify-center mr-2'>
+                <GithubLogo 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary'/>
+            </a>  
+            <a 
+              target="blank"
+              href="https://esquecimeucep.netlify.app/"
+              className='flex justify-center'>
+                <Browsers 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary'/>
+            </a>
+          </div>                               
+        </div>
+        <div className='project-list-container group'>
+          <Card.CardImage
+            src={GithubGif} 
+            className='w-22 h-22 border-2 border-primary rounded-full mb-4' 
+          />
+          <Card.CardTitle 
+            className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
+            title="Github Repo Filter"
+          />                                  
+          <div className='flex'>
+            <a 
+              target="blank"
+              href="https://github.com/gabrigomez/github-repo-finder"
+              className='flex justify-center mr-2'>
+                <GithubLogo 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary'/>
+            </a>
+            <a 
+              target="blank"
+              href="https://finderrepo.netlify.app/"
+              className='flex justify-center'>
+                <Browsers 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary'/>
+            </a>
+          </div>                                         
+        </div>        
+        <div className='project-list-container group'>
+          <Card.CardImage
+            src={Pacman} 
+            className='w-22 h-22 border-2 border-primary rounded-full mb-4' 
+          />
+          <Card.CardTitle 
+            className='text-sm sm:text-xl md:text-2xl font-bold opacity-0 group-hover:opacity-100 duration-700'
+            title="My classic games"
+          />              
+          <div className='flex'>            
+            <a 
+              target="blank"
+              href="https://github.com/gabrigomez/my-classic-games"
+              className='flex justify-center'>
+                <GithubLogo 
+                  className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                  text-center hover:text-black dark:hover:text-primary mr-2'/>
+            </a>
+            <a 
+              target="blank"
+              href="https://myclassicgamelist.netlify.app/"
+              className='flex justify-center'>
+              <Browsers 
+                className='text-2xl mt-4 opacity-0 group-hover:opacity-100 duration-200 
+                text-center hover:text-black dark:hover:text-primary'/>
+            </a>
+          </div>                     
         </div>        
       </Carousel>
     </Card.Root>
