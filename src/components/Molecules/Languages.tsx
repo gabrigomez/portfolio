c9554b6c5b50f1dfccf55bb9fcb3be3aa49433d9
Gabriel <gabrigomez15@gmail.com>
2023-08-25 14:20:31 -0300

    fix: scale on Interests icons

--- src/components/Molecules/Languages.tsx
+++ src/components/Molecules/Languages.tsx
@@ -11,14 +11,14 @@ export const Languages = ({isVisible} : LanguagesProps) => {
   return (
     <div className={isVisible ? "animate__animated animate__fadeIn" : "none"}>
       <div className='flex justify-center' id='icons'>
-        <SiReact className='text-4xl lg:text-6xl mr-2 lg:mr-6' />
-        <SiVuedotjs className='text-4xl lg:text-6xl mr-2 lg:mr-6' />
-        <FaNodeJs className='text-4xl lg:text-6xl mr-2 lg:mr-6' />
-        <SiTypescript className='text-4xl lg:text-6xl mr-2 lg:mr-6' />           
-        <SiTailwindcss className='text-4xl lg:text-6xl mr-2 lg:mr-6' />
-        <SiCypress className='text-4xl lg:text-6xl mr-2 lg:mr-6' />
-        <SiDjango className='text-4xl lg:text-6xl mr-2 lg:mr-6' />
-        <SiRubyonrails className='text-4xl lg:text-6xl mr-2 lg:mr-6' />
+        <SiReact className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiVuedotjs className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <FaNodeJs className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiTypescript className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />           
+        <SiTailwindcss className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiCypress className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiDjango className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiRubyonrails className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
       </div>
     </div>
   )
