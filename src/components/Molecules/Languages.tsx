1e7f82a51775f96b628a722365e299050b487aae
Gabriel <gabrigomez15@gmail.com>
2023-08-31 09:56:23 -0300

    styles: fix Interests badges responsiveness

--- src/components/Molecules/Languages.tsx
+++ src/components/Molecules/Languages.tsx
@@ -11,14 +11,14 @@ export const Languages = ({isVisible} : LanguagesProps) => {
   return (
     <div className={isVisible ? "animate__animated animate__fadeIn" : "none"}>
       <div className='flex justify-center' id='icons'>
-        <SiReact className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
-        <SiVuedotjs className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
-        <FaNodeJs className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
-        <SiTypescript className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />           
-        <SiTailwindcss className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
-        <SiCypress className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
-        <SiDjango className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
-        <SiRubyonrails className='text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiReact className='text-3xl sm:text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiVuedotjs className='text-3xl sm:text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <FaNodeJs className='text-3xl sm:text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiTypescript className='text-3xl sm:text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />           
+        <SiTailwindcss className='text-3xl sm:text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiCypress className='text-3xl sm:text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiDjango className='text-3xl sm:text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
+        <SiRubyonrails className='text-3xl sm:text-4xl lg:text-6xl mr-2 lg:mr-6 cursor-pointer hover:scale-110 duration-200' />
       </div>
     </div>
   )
